f2fbc69cf57fe15b2c8a5bedebc82a549b38efb2
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-11-12 13:59:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+<script>
+function myFunction() {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "Pictures/Mort.jpg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "Morty");
+  document.body.appendChild(x);
+}
+</script>
