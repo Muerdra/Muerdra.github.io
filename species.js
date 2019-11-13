f467fe2fb6b2db3cb941ed478e10c2aa22becfd6
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-11-13 11:14:33 -0500

    Update species.js

--- species.js
+++ species.js
@@ -1,19 +1,23 @@
-                  
-
-                                                /*https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tab_header*/
-function openCity(cityName,elmnt,color) {
+function openCity(cityName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
+  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-  document.getElementById(cityName).style.display = "block";
-  elmnt.style.backgroundColor = color;
 
+  // Show the specific tab content
+  document.getElementById(cityName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
 }
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
