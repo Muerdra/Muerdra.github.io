eaf0a56de1bdfd86cf4089c49b620701be49acb2
resherwi <57364537+resherwi@users.noreply.github.com>
2019-11-19 15:52:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 	function indexbutton() {
   			var x = document.createElement("IMG");
- 			x.setAttribute("src", "Pictures/maptest");
+ 			x.setAttribute("src", "Pictures/maptest.jpg");
   			x.setAttribute("width", "304");
   			x.setAttribute("height", "228");
   			x.setAttribute("alt", "");
