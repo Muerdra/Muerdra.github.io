5e52c7b7153ecb9e707cefd9e417283d571f7616
resherwi <57364537+resherwi@users.noreply.github.com>
2019-11-19 15:12:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
-function myFunction() {
-  var x = document.createElement("IMG");
-  x.setAttribute("src", "Pictures/Mort.jpg");
-  x.setAttribute("width", "304");
-  x.setAttribute("height", "228");
-  x.setAttribute("alt", "Morty");
-  document.body.appendChild(x);
-}
+	function indexbutton() {
+  			var x = document.createElement("IMG");
+ 			x.setAttribute("src", "Pictures/island.jpg");
+  			x.setAttribute("width", "304");
+  			x.setAttribute("height", "228");
+  			x.setAttribute("alt", "");
+  			document.body.appendChild(x);
+		}
