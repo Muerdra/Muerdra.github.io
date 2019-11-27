5145ff44eb90c895f3d9726cce9511dbacc64098
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-11-27 09:20:38 -0500

    Update species.js

--- species.js
+++ species.js
@@ -31,3 +31,13 @@ function openCity(cityName, elmnt, color) {
                 document.getElementById("defaultOpen").click();
 
 }
+
+//TESTING
+function indexbutton() {
+  			var x = document.createElement("IMG");
+ 			  x.setAttribute("src", "Pictures/maptest.jpg");
+  			x.setAttribute("width", "304");
+  			x.setAttribute("height", "228");
+  			x.setAttribute("alt", "	SHOW ME PIC");
+  			document.body.appendChild(x);
+		}
