bb41f5724e20c1e7adb19eb348ab3d693ccc796f
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-12-06 12:22:15 -0500

    Update species.js

--- species.js
+++ species.js
@@ -1,12 +1,14 @@
 //
 //Craig
-//Opens the defaultOpen tab on pageload(Animal1)
-//Used as reference: https://forums.asp.net/t/2113496.aspx?how+do+I+make+a+tab+to+open+by+default+on+clicking+the+div
+//Opens the tab defaultOpen tab on pageload(Animal1)
+//Used as reference/template: https://forums.asp.net/t/2113496.aspx?how+do+I+make+a+tab+to+open+by+default+on+clicking+the+div
 window.onload = function () {
   startTab();
 };
 
-//Craig
+//Used as Template https://www.w3schools.com/howto/howto_js_tab_header.asp-->
+//Tab section Craig Harding -->
+
 function openAnimal(aniName, elmnt, color) {
   // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
@@ -14,14 +16,11 @@ function openAnimal(aniName, elmnt, color) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-//Craig
   // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-
   // Show the specific tab content
   document.getElementById(aniName).style.display = "block";
 
@@ -29,8 +28,11 @@ function openAnimal(aniName, elmnt, color) {
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
+
+//Craig 
+//Get the element with id="defaultOpen" and click on it
 //added startTab function to defaultOpen
+//Used as reference: https://forums.asp.net/t/2113496.aspx?how+do+I+make+a+tab+to+open+by+default+on+clicking+the+div
  function startTab() {
                 document.getElementById("defaultOpen").click();
 
