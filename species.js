a925b7ea4a9840ba1c4bda14f77ae16dbcf6736c
Muerdra <56932817+Muerdra@users.noreply.github.com>
2019-12-06 15:13:50 -0500

    Update species.js

--- species.js
+++ species.js
@@ -4,6 +4,11 @@
 window.onload = function () {
   startTab();
 }
+/*Whatever is named defaultOpen on tab will open when the user goes to a page with tabs
+*/
+ function startTab() {
+                document.getElementById("defaultOpen").click();
+}
 
 /*Used as Template https://www.w3schools.com/howto/howto_js_tab_header.asp-->
 //Tab section Craig Harding */
@@ -19,7 +24,7 @@ function openAnimal(aniName, elmnt, color) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-  /* Shows the specific tab content*/
+  /* Shows the wanted tab content on click*/
   document.getElementById(aniName).style.display = "block";
 
   /* Add the specific color to the button used to open the tab content*/
@@ -27,10 +32,4 @@ function openAnimal(aniName, elmnt, color) {
 }
 
 
-/*Craig 
-//Get the element with id="defaultOpen" and click on it
-//added startTab function to defaultOpen
-//Used as reference: https://forums.asp.net/t/2113496.aspx?how+do+I+make+a+tab+to+open+by+default+on+clicking+the+div */
- function startTab() {
-                document.getElementById("defaultOpen").click();
-}
+
